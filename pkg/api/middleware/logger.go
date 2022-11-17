152ce3a937d98d43cc490abca83693bdba1c3cd1
sturm19 <sturm@sub.uni-goettingen.de>
2022-11-17 11:00:07 +0100

    Fix case usage in logger fields

--- pkg/api/middleware/logger.go
+++ pkg/api/middleware/logger.go
@@ -18,8 +18,8 @@ type APILogger struct {
 func Logger(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		customLogger := logrus.WithFields(logrus.Fields{
-			"RequestID":    c.Response().Header()[echo.HeaderXRequestID],
-			"usertracking": c.Request().Header.Get(HEADER_USER_TRACKING),
+			"requestID":    c.Response().Header()[echo.HeaderXRequestID],
+			"userTracking": c.Request().Header.Get(HEADER_USER_TRACKING),
 		})
 		c.Set(LOGGER_KEY, APILogger{customLogger})
 		return next(c)
