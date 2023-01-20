859e311fd97ae9d1c9fd069c01bb818fb98ce8ec
sturm19 <sturm@sub.uni-goettingen.de>
2023-01-20 09:10:00 +0100

    Improve output format of fulldataquery

--- pkg/model/fullData.go
+++ pkg/model/fullData.go
@@ -4,22 +4,22 @@ type FullData struct {
 	Sample_Num       int
 	UniqueId         string `json:"unique_id"`
 	Batches          []int
-	References       interface{}
-	SampleIds        []string
-	Location_Names   [][]string
-	Location_Types   [][]string
+	References       []interface{}
+	SampleName       string
+	Location_Names   []string
+	Location_Types   []string
 	Loc_Data         interface{}
-	Elevation_Min    int
-	Elevation_Max    int
-	Land_Or_Sea      [][]string
-	Rock_Type        string
-	Rock_Class       string
-	Rock_Texture     string
+	Elevation_Min    string
+	Elevation_Max    string
+	Land_Or_Sea      string
+	Rock_Types       []string
+	Rock_Classes     []string
+	Rock_Textures    []string
 	Age_Min          int
 	Age_Max          int
-	Material         []string
-	Mineral          []string
-	Inclusion_Type   []string
+	Materials        []string
+	Minerals         []string
+	Inclusion_Types  []string
 	Location_Num     int
 	Latitude         float32
 	Longitude        float32
@@ -27,14 +27,14 @@ type FullData struct {
 	Latitude_Max     string
 	Longitude_Min    string
 	Longitude_Max    string
-	Tectonic_Setting [][]string
-	Method           []string
-	Comment          []string
-	Institution      [][]string
-	Item_Name        [][]string
-	Item_Group       [][]string
-	Standard_Names   [][]string
-	Standard_Values  [][]float32
-	Values           [][]float32
-	Units            [][]string
+	Tectonic_Setting string
+	Method           string
+	Comment          string
+	Institution      string
+	Item_Name        []string
+	Item_Group       []string
+	Standard_Names   []string
+	Standard_Values  []float32
+	Values           []float32
+	Units            []string
 }
