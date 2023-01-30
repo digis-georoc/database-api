5b0170732a9f74c098ceed96b2c5f9c3ec7cb8cb
sturm19 <sturm@sub.uni-goettingen.de>
2023-01-30 12:39:05 +0100

    Fix missing name alias for samplingtechniques

--- pkg/sql/annotations.go
+++ pkg/sql/annotations.go
@@ -1,7 +1,7 @@
 package sql
 
 const SamplingTechniquesQuery = `
-select distinct a.annotationtext 
+select distinct a.annotationtext as name
 from odm2.annotations a 
 where a.annotationcode = 'g_samples.samp_technique'
 `
