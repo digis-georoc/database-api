1414934ee0afec93ec496eb4cd58014a97dc163e
sturm19 <sturm@sub.uni-goettingen.de>
2023-05-11 12:05:43 +0200

    Increment api version to 0.2.1

--- docs/docs.go
+++ docs/docs.go
@@ -2373,7 +2373,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.2.0",
+	Version:          "0.2.1",
 	Host:             "api-test.georoc.eu",
 	BasePath:         "/api/v1",
 	Schemes:          []string{"https", "http"},
