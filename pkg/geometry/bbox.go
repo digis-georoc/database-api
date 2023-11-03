23948cd9b2e3c5b223feada9fd8a9a686c67b331
sturm19 <sturm@sub.uni-goettingen.de>
2023-11-03 09:09:29 +0100

    Move bbox and coordinate functions to its own package

---
+++ pkg/geometry/bbox.go
@@ -0,0 +1,71 @@
+/**
+** This file contains helper functions for handling bounding boxes
+**/
+package geometry
+
+import "math"
+
+const (
+	LONG_MIN = -180.0
+	LONG_MAX = 180.0
+	LAT_MIN  = -90.0
+	LAT_MAX  = 90.0
+)
+
+// IsZoom0 returns true if the given bbox is big enough to fit the whole world view (zoom = 0); false if it is smaller.
+func IsZoom0(bbox [][]float64) bool {
+	// check if height of bbox is >= |LAT_MIN| + LAT_MAX and width of bbox is |LONG_MIN| + LONG_MAX
+	return bbox[3][1]-bbox[0][1] >= math.Abs(LAT_MIN)+LAT_MAX && bbox[1][0]-bbox[0][0] >= math.Abs(LONG_MIN)+LONG_MAX
+}
+
+// ScaleBbox takes an array of coordinates for a bounding box and scales it around the center
+// Scales latitudes up to LAT_MIN/LAT_MAX only
+// Input is 2-dimensional array of points formatted: [[long1,lat1],[long2,lat2],...]
+func ScaleBBox(bbox [][]float64) [][]float64 {
+	// calc width and height of bbox
+	width := bbox[1][0] - bbox[0][0]
+	height := bbox[3][1] - bbox[0][1]
+	scaleLong := width / 2
+	scaleLat := height / 2
+	// add half bbox on each side
+	// SW
+	bbox[0][0] = bbox[0][0] - scaleLong
+	bbox[0][1] = math.Max(LAT_MIN, bbox[0][1]-scaleLat)
+	// SE
+	bbox[1][0] = bbox[1][0] + scaleLong
+	bbox[1][1] = math.Max(LAT_MIN, bbox[1][1]-scaleLat)
+	// NE
+	bbox[2][0] = bbox[2][0] + scaleLong
+	bbox[2][1] = math.Min(LAT_MAX, bbox[2][1]+scaleLat)
+	// NW
+	bbox[3][0] = bbox[3][0] - scaleLong
+	bbox[3][1] = math.Min(LAT_MAX, bbox[3][1]+scaleLat)
+	return bbox
+}
+
+// TruncateBBox truncates a given bbox to at most 360 width and 180 height by reducing the size equally from both sides
+func TruncateBBox(bbox [][]float64) [][]float64 {
+	width := bbox[1][0] - bbox[0][0]
+	height := bbox[3][1] - bbox[0][1]
+	if width <= LONG_MAX*2 && height <= LAT_MAX*2 {
+		// no need to truncate
+		return bbox
+	}
+	// calc middle longitude and latitude
+	middleLong := (bbox[1][0] + bbox[0][0]) / 2
+	middleLat := (bbox[3][1] + bbox[0][1]) / 2
+	// truncate bbox by keeping only middle +- LONG/LAT_MAX
+	// SW
+	bbox[0][0] = math.Max(bbox[0][0], middleLong-LONG_MAX)
+	bbox[0][1] = math.Max(bbox[0][1], middleLat-LAT_MAX)
+	// SE
+	bbox[1][0] = math.Min(bbox[1][0], middleLong+LONG_MAX)
+	bbox[1][1] = math.Max(bbox[1][1], middleLat-LAT_MAX)
+	// NE
+	bbox[2][0] = math.Min(bbox[2][0], middleLong+LONG_MAX)
+	bbox[2][1] = math.Min(bbox[2][1], middleLat+LAT_MAX)
+	// NW
+	bbox[3][0] = math.Max(bbox[3][0], middleLong-LONG_MAX)
+	bbox[3][1] = math.Min(bbox[3][1], middleLat+LAT_MAX)
+	return bbox
+}
---
+++ pkg/geometry/coordinates.go
@@ -0,0 +1,72 @@
+/**
+** This file contains helper functions for coordinate data
+**/
+package geometry
+
+import (
+	"encoding/json"
+	"fmt"
+	"math"
+)
+
+// FormatPolygonArray formats a given input polygon for usage in postGIS/SQL syntax
+// Input is 2-dimensional array of points formatted: [[long1,lat1],[long2,lat2],...]
+// Output is postGIS geometry syntax: (long1 lat1, long2 lat2, ...)
+func FormatPolygonArray(polygon [][]float64) (string, error) {
+	output := "("
+	for i, point := range polygon {
+		if i > 0 {
+			// add separator before adding next point
+			output += ","
+		}
+		for _, coordinate := range point {
+			output += fmt.Sprintf(" %f", coordinate)
+		}
+	}
+	output += ")"
+	return output, nil
+}
+
+// ParsePointArray parses a string representation of an array of float-points into a 2-dimensional array
+func ParsePointArray(arrayString string) ([][]float64, error) {
+	polygon := [][]float64{}
+	err := json.Unmarshal([]byte(arrayString), &polygon)
+	if err != nil {
+		return nil, err
+	}
+	return polygon, nil
+}
+
+// CalcTranslation calculates the longitudinal translation and crossed bound of a polygon
+// Input is 2-dimensional array of points formatted: [[long1,lat1],[long2,lat2],...]
+// Max polygon size is limited to 180 height and 360 width. Scale polygon accordingly before these calculations.
+func CalcTranslation(polygon [][]float64) (float64, float64, error) {
+	var left, right, top, bottom float64
+	for i, point := range polygon {
+		if i == 0 || left > point[0] {
+			left = point[0]
+		}
+		if i == 0 || right < point[0] {
+			right = point[0]
+		}
+		if i == 0 || top < point[1] {
+			top = point[1]
+		}
+		if i == 0 || bottom > point[1] {
+			bottom = point[1]
+		}
+	}
+	if right-left > 360 || top-bottom > 180 {
+		return 0, 0, fmt.Errorf("Polygon dimensions out of bounds")
+	}
+	// since max width is 360, all points have the same translation (postGIS' wrapx handles cases where part of the bbox is within the bounds an thus should have a factor of 0)
+	// and only one boundary can be crossed (-180 or +180)
+	boundary := 180.0
+	translationFactor := -math.Floor((right + 180) / 360)
+	if left < -180 {
+		boundary = -180.0
+		translationFactor = -math.Floor((left + 180) / 360)
+	}
+
+	return boundary, translationFactor, nil
+}
