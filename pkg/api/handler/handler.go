1b1a0df2162a44fa48f4b40223edf13eb6292540
sturm19 <sturm@sub.uni-goettingen.de>
2023-11-29 17:36:02 +0100

    Change ping endpoint to ping the db; Add alive endpoint to check api

--- pkg/api/handler/handler.go
+++ pkg/api/handler/handler.go
@@ -32,16 +32,33 @@ func NewHandler(db repository.PostgresConnector, config *middleware.KeycloakConf
 }
 
 // Ping godoc
-// @Summary     Sample request
+// @Summary     Health request to check db connection
+// @Description Check connection to db
+// @Tags        general
+// @Accept      json
+// @Produce     json
+// @Success     200 {object} string
+// @Failure     424 {object} string
+// @Router      /ping [get]
+func (h *Handler) Ping(c echo.Context) error {
+	err := h.db.Ping()
+	if err != nil {
+		return c.JSON(http.StatusFailedDependency, err.Error())
+	}
+	return c.JSON(http.StatusOK, "Pong")
+}
+
+// Alive godoc
+// @Summary     Health request to check if api is responsive
 // @Description Check connection to api
 // @Tags        general
 // @Accept      json
 // @Produce     json
 // @Success     200 {object} string
 // @Failure     404 {object} string
-// @Router      /ping [get]
-func (h *Handler) Ping(c echo.Context) error {
-	return c.JSON(http.StatusOK, "Pong")
+// @Router      /alive [get]
+func (h *Handler) Alive(c echo.Context) error {
+	return c.JSON(http.StatusOK, "I'm still alive...")
 }
 
 // Version godoc
